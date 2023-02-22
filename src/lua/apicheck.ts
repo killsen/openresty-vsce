d0af884e09be591e9fbf5032583125bedeca616e
killsen <killsen@qq.com>
2023-02-22 15:13:10 +0800

    新增类型检查缓存

--- src/lua/apicheck.ts
+++ src/lua/apicheck.ts
@@ -4,6 +4,8 @@ import * as ngx from './ngx';
 import { loadModuleByCode } from './modLoader';
 import { callFunc } from './modFunc';
 
+// 类型检查缓存
+const CACHE = new Map<string, vscode.Diagnostic[]>();
 
 /** 注册 ApiCheck */
 export function registerApiCheck(context: vscode.ExtensionContext) {
@@ -12,25 +14,33 @@ export function registerApiCheck(context: vscode.ExtensionContext) {
     const selector = { language: 'lua', scheme: 'file' };
     const subscriptions = context.subscriptions;
 
-    function clear (document: vscode.TextDocument) {
-        if (vscode.languages.match(selector, document)) {
-            const uri = document.uri;
-            collection.set(uri, []);
-        }
+    function onDidClose (document: vscode.TextDocument) {
+        if (!vscode.languages.match(selector, document)) {return;}
+        collection.set(document.uri, []);
     }
 
-    function lint (doc: vscode.TextDocument) {
-        if (!vscode.languages.match(selector, doc)) {return;}
+    function onDidChange (document: vscode.TextDocument) {
+        if (!vscode.languages.match(selector, document)) {return;}
+        CACHE.delete(document.fileName);
+        collection.set(document.uri, []);
+    }
 
-        const uri = doc.uri;
-        const diagnostics: vscode.Diagnostic[] = [];
+    function lint (document: vscode.TextDocument) {
+        if (!vscode.languages.match(selector, document)) {return;}
 
-        let ctx = ngx.getPath(doc.fileName);
-        ctx.fileName = doc.fileName + ".editing";  // 正在编辑中的文件
+        const { uri, fileName } = document;
+
+        let ctx = ngx.getPath(fileName);
+        ctx.fileName = fileName + ".editing";  // 正在编辑中的文件
+
+        if (CACHE.has(fileName)) {
+            collection.set(uri, CACHE.get(fileName));
+            return;
+        }
 
         let $lints: any[] = [];
         let $funcs: any[] = [];
-        let docText = doc.getText();
+        let docText = document.getText();
 
         loadModuleByCode(ctx, docText, "", $lints, $funcs);
 
@@ -41,13 +51,14 @@ export function registerApiCheck(context: vscode.ExtensionContext) {
             }
         });
 
+        CACHE.set(fileName, $lints);
         collection.set(uri, $lints);
     }
 
     vscode.workspace.onDidOpenTextDocument(document => lint(document), null, subscriptions);
     vscode.workspace.onDidSaveTextDocument(document => lint(document), null, subscriptions);
-    vscode.workspace.onDidCloseTextDocument(document => clear(document), null, subscriptions);
-    vscode.workspace.onDidChangeTextDocument(change => clear(change.document), null, subscriptions);
+    vscode.workspace.onDidCloseTextDocument(document => onDidClose(document), null, subscriptions);
+    vscode.workspace.onDidChangeTextDocument(change => onDidChange(change.document), null, subscriptions);
     vscode.window.onDidChangeActiveTextEditor(editor => editor && lint(editor.document), null, subscriptions);
 
     if (vscode.window.activeTextEditor) {
