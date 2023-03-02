27c82065d1185ec796b0229f1eb0801dd5e9ee80
killsen <killsen@qq.com>
2023-03-02 16:08:49 +0800

    优化 dao 接口

--- src/lua/modRequirer.ts
+++ src/lua/modRequirer.ts
@@ -1,7 +1,7 @@
 
 import { NgxPath, getApiFile } from './ngx';
 import { loadApiDoc } from './apiDoc';
-import { LuaModule, LuaDao, LuaApi, getBasicType, LuaAny, LuaString } from './types';
+import { LuaModule, LuaDao, LuaApi, getBasicType, LuaAny, LuaString, LuaNumber, LuaNever, LuaType } from './types';
 import { setDepend } from "./modCache";
 import { isObject, setItem } from './utils';
 import { TableLib } from './libs/TableLib';
@@ -13,7 +13,7 @@ const readonly = true;
 const basic = true;
 
 /** 获取单个返回值类型 */
-function genValue(args: string, _g: LuaScope, loc: LuaApi["loc"]): any {
+function genValue(args: string, _g: LuaScope, loc?: LuaApi["loc"]): any {
 
     let vt = getBasicType(args) || _g[args];
     if (vt) { return vt; }
@@ -28,7 +28,7 @@ function genValue(args: string, _g: LuaScope, loc: LuaApi["loc"]): any {
 }
 
 /** 获取参数类型或者返回值类型 */
-function genArgs(args: string, _g: LuaScope, loc: LuaApi["loc"], isRes = false): any[] {
+function genArgs(args: string, _g: LuaScope, loc?: LuaApi["loc"], isRes = false): any[] {
 
     // 去除空格及左右两边的小括号()
     args = args.replace(/\s/g, "");
@@ -76,6 +76,113 @@ function genArgs(args: string, _g: LuaScope, loc: LuaApi["loc"], isRes = false):
     });
 }
 
+const $dao_ext = {
+    _order_by : { type: "string", basic, readonly, doc: "## _order_by \n\n `< string >` \n\n ### 排序 \n\n" },
+    _group_by : { type: "string", basic, readonly, doc: "## _group_by \n\n `< string >` \n\n ### 汇总 \n\n" },
+    _limit    : { type: "number | string", types: [ LuaNumber, LuaString ], readonly, doc: "## _limit    \n\n `< number | string >` \n\n ### 记录数 \n\n" },
+};
+
+// 注入 dao 类型
+function initDao(_g: LuaScope, dao: LuaDao) {
+
+    _g["row"] = {
+        "." : dao.row, "[]": LuaNever,
+        type: "$"+ dao.name, readonly,
+        doc : "## $"+ dao.name +"\ndao 类型单行数据\n" + dao.doc,
+    };
+
+    _g["row[]"] = {
+        "." : {}, "[]": _g["row"],
+        type: "$"+ dao.name + "[]", readonly,
+        doc: "## $"+ dao.name +"[]\ndao 类型多行数据\n" + dao.doc,
+    };
+
+    _g["sql"] = {
+        type: "string", readonly, basic,
+        doc: "sql操作语句"
+    };
+
+    _g["sql_query"] = {
+        type: "string", readonly, basic,
+        $result: _g["row[]"],
+        doc: "sql查询语句",
+    };
+
+    _g["rowx"] = {
+        "." : { ...dao.row, ...$dao_ext },
+        type: "$"+ dao.name, readonly,
+    };
+
+    _g["query"] = genValue("rowx | string[] | map<string>[]", _g);
+
+    _g["update"] = genValue("row | string[] | (row | string[])[]", _g);
+
+    _g["where"] = genValue("row | string[]", _g);
+
+}
+
+/** 生成指定字段查询结果 */
+function gen_dao_fields(t: any, dao: LuaDao) : LuaType | undefined {
+
+    if (!isObject(t) || !isObject(t["."])) { return; }
+
+    for (let i=1; i<100; i++) {
+        let f = t["."][i];
+        if (f === null || f === undefined) { return; }
+        if (!isObject(f["."])) { continue; }
+
+        const ti = {} as any;
+
+        for (let k in f["."]) {
+            const v = f["."][k];
+            if (Number(k)) {
+                typeof v === "string" && v.split(",").forEach(name => {
+                    name = name.trim();
+                    let namex = name;
+                    if (name.includes(" as ")) {
+                        let arr = name.split(" as ");
+                        namex = arr[0].trim();
+                        name  = arr[1].trim();
+                    }
+                    ti[name] = dao.row[namex] || LuaAny;
+                    ti["$" + name + "$"] = f["."]["$" + k + "$"];
+                });
+            } else if (!k.startsWith("$")) {
+                ti[k] = dao.row[v] || LuaAny;
+                ti["$" + k + "$"] = f["."]["$" + k + "$"];
+            }
+        }
+
+        return { type: "table", readonly, ".": ti };
+    }
+
+}
+
+/** 生成 dao 的 get 及 list 方法 */
+function gen_dao_func(mod: LuaModule, dao: LuaDao, $row: LuaType, $rows: LuaType) {
+
+    setItem(mod, [".", "get", "()"], (t: any) => {
+        return gen_dao_fields(t, dao) || $row;
+    });
+
+    setItem(mod, [".", "list", "()"], (t: any) => {
+        const row = gen_dao_fields(t, dao);
+        return row && { "[]": row, type: "table[]", readonly } || $rows;
+    });
+
+    setItem(mod, [":", "get" , "()"], gen_sql_query);
+    setItem(mod, [":", "list", "()"], gen_sql_query);
+
+    function gen_sql_query(t: any) {
+        const row = gen_dao_fields(t, dao);
+        return {
+            type: "string", readonly, basic,
+            $result: row && { "[]": row, type: "table[]", readonly } || $rows,
+            doc: "sql查询语句",
+        };
+    }
+
+}
 
 /** 通过API文件加载接口声明 */
 export function requireModule(ctx: NgxPath, name: string, dao?: LuaDao): LuaModule | undefined {
@@ -90,32 +197,8 @@ export function requireModule(ctx: NgxPath, name: string, dao?: LuaDao): LuaModu
     let _g: LuaScope = { $local: {}, $scope: undefined, $file: fileName };
     let requireName = "";
 
-    let daoDoc = "";
-    if (dao) {
-        // 注入 dao 类型
-        daoDoc = dao.doc;
-        _g["row"] = {
-            type: "$"+ dao.name,
-            "." : dao.row, readonly,
-            doc : "## $"+ dao.name +"\ndao 类型单行数据\n" + daoDoc,
-        };
-        _g["row[]"] = {
-            type: "$"+ dao.name + "[]",
-            "[]": _g["row"], readonly,
-            doc: "## $"+ dao.name +"[]\ndao 类型多行数据\n" + daoDoc,
-        };
-        _g["sql"] = {
-            type: "string",
-            readonly, basic,
-            doc: "sql操作语句",
-        };
-        _g["sqls"] = {
-            type: "string",
-            readonly, basic,
-            $result: _g["row[]"],
-            doc: "sql查询语句",
-        };
-    }
+    let daoDoc = dao?.doc || "";
+    dao && initDao(_g, dao);  // 注入 dao 类型
 
     function genNode(api: LuaApi) {
         // console.log(api.name);
@@ -187,10 +270,6 @@ export function requireModule(ctx: NgxPath, name: string, dao?: LuaDao): LuaModu
             p.doc   = api.doc + daoDoc;
             p.$file = api.file;
             p.$loc  = api.loc;
-
-            if (p.args && p.args !== "()" && dao) {
-                p.$dao = dao; // 用于 dao 补全
-            }
 
         } else if (api.res) {
             if (!isNaN(Number(api.res))) {
@@ -276,7 +355,12 @@ export function requireModule(ctx: NgxPath, name: string, dao?: LuaDao): LuaModu
         }
         t["$file"] = dao["$file"];
         t["$dao"] = dao;
+        t["$row"] = _g["row"];
         t["doc"] = daoDoc;
+
+        // 生成 dao 的 get 及 list 方法
+        gen_dao_func(t, dao, _g["row"], _g["row[]"]);
+
     } else if (t instanceof Object) {
         t["$file"] = fileName;
     }
